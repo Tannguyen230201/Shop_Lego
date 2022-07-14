613b2e27aaebf059781c2f5c0fa1432d77edd6fa
Tan Nguyen <nguyenvantan230201@gmail.com>
2022-07-14 15:19:22 +0700

    Web demo

---
+++ main.js
@@ -0,0 +1,19 @@
+function back_here()
+{
+    window.location="http://127.0.0.1:5500/";
+}
+function btn_fb()
+{
+    window.location="https://www.facebook.com/";
+}
+function btn_google()
+{
+    window.location="https://www.google.com/";
+}
+//Modal
+
+const modal=document.querySelector('.modal')
+// function show(){
+    alert('hello');
+// }
+// btn_back.addEventListener('click',show);
